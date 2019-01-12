e95ad8888bff0b39fec326b30ec0fcf2555b8577
johnta0 <22534726+johnta0@users.noreply.github.com>
2019-01-12 21:38:44 +0900

    add simple test for NAND

--- neural_network/perceptron.py
+++ neural_network/perceptron.py
@@ -16,3 +16,21 @@ def NAND(x1, x2):
     if AND(x1, x2) == 1:
         return 0
     return 1
+
+def test_NAND():
+    test_data = [
+        [0, 0, 1],
+        [0, 1, 1],
+        [1, 0, 1],
+        [1, 1, 0]
+       ]
+    for d in test_data:
+        if d[2] != NAND(d[0], d[1]):
+            return False
+    return True
+    
+if __name__ == '__main__':
+    if not test_NAND():
+        print("Test failed..")
+    else:
+        print("Test success!")
