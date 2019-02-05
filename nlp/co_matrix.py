c3242ac5f1c3c95ba9eeffbf88d79083742a5d03
johnta0 <22534726+johnta0@users.noreply.github.com>
2019-02-05 17:24:08 +0900

    impl cosine similarity

--- nlp/co_matrix.py
+++ nlp/co_matrix.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import numpy as np
-from common.util import preprocess
+from common.util import preprocess, cos_similarity
 
 def create_co_occurence_matrix(corpus, vocab_size, window_size=1):
 
@@ -30,3 +30,13 @@ if __name__ == '__main__':
     print("corpus:", corpus)
     co_matrix = create_co_occurence_matrix(corpus, len(word_to_id))
     print(co_matrix)
+
+    # cosine similarity を計算してみる
+    print('cosine similarities...')
+    vec_you = co_matrix[word_to_id['you']]
+    vec_I = co_matrix[word_to_id['i']]
+    print('you and i:', cos_similarity(vec_you, vec_I))
+
+    vec_hello = co_matrix[word_to_id['hello']]
+    print('you and hello:', cos_similarity(vec_you, vec_hello))
+    print('i and hello:', cos_similarity(vec_I, vec_hello))
--- nlp/common/util.py
+++ nlp/common/util.py
@@ -15,3 +15,7 @@ def preprocess(text):
     corpus = np.array([word_to_id[w] for w in words])
     return corpus, word_to_id, id_to_word
 
+def cos_similarity(x, y, eps=1e-8):
+    nx = x / (np.sqrt(np.sum(x ** 2)) + eps)
+    ny = y / (np.sqrt(np.sum(y ** 2)) + eps)
+    return np.dot(nx, ny)
