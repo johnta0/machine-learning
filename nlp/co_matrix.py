4f04f7976231701ec588eddf6da834e69dbef325
johnta0 <22534726+johnta0@users.noreply.github.com>
2019-02-05 16:23:25 +0900

    impl method to create cooccurence matrix

---
+++ nlp/co_matrix.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from common.util import preprocess
+
+def create_co_occurence_matrix(corpus, vocab_size, window_size=1):
+
+    """
+        corpus: list of word id (e.g., [0 1 2 3 4 5 1 5 6] for "you say goodbye and I say hello .").
+    """
+    corpus_size = len(corpus)
+    matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32) # initialize 2d array by zero
+
+    for idx, word_id in enumerate(corpus):
+        for i in range(1, window_size + 1):
+            left_idx = idx - i
+            right_idx = idx + i
+
+            if left_idx >= 0:
+                left_word_id = corpus[left_idx]
+                matrix[word_id, left_word_id] += 1
+            if right_idx < corpus_size:
+                right_word_id = corpus[right_idx]
+                matrix[word_id, right_word_id] += 1
+    return matrix
+
+if __name__ == '__main__':
+    text = "You say goodbye and I say hello."
+    corpus, word_to_id, id_to_word = preprocess(text)
+    print("corpus:", corpus)
+    co_matrix = create_co_occurence_matrix(corpus, len(word_to_id))
+    print(co_matrix)
